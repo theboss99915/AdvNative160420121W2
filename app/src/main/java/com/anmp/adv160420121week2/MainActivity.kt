4cfb1030250f8768f8d2fe6db3822679d042c6a4
ASUS1 <paddyoop@gmail.com>
2023-03-08 13:07:04 +0700

    innit

---
+++ app/src/main/java/com/anmp/adv160420121week2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.anmp.adv160420121week2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
